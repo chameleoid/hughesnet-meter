bdf1fe06fbe0007e60aeb60d581063618cfa9722
rummik <r@9k1.us>
2013-06-25 02:27:27 -0400

    Update to work with the new download allowance page

--- content/background.js
+++ content/background.js
@@ -36,27 +36,45 @@
 		if (xhr.readyState != 4 || xhr.status != 200)
 			return;
 
-		$('.hnb')[0].innerHTML = xhr.responseText.replace(/<meta[^>]+>/g, '');
+		$('.hnb')[0].innerHTML = xhr.responseText.replace(/<(meta|img|link)[^>]+>/g, '');
 
 		var offset, hue, t,
-		    v = 40,
+		    v    = 40,
+		    key  = [ 'throttled', 'maximum', 'daily', 'remaining' ],
+		    data = {};
 
-		    limited = /download speed limited/.test(xhr.responseText),
+		$('.hnb #info_table td:nth-child(2)').map(function(e, i) {
+			var t = e.innerHTML.replace(/^\s+|\s+$/g, '');
 
-		    data = $('.hnb table td:nth-child(2) td:nth-child(3)').map(function(e, i) {
-			    var t = e.innerHTML.replace(/^\s+|\s+$/g, '');
+			switch (key[i]) {
+				case 'throttled':
+					t = (t != 'Unthrottled'); break;
 
-			    // return first three values in integer form
-			    if (i < 3)
-			    	return +t;
+				case 'maximum': case 'daily': case 'remaining':
+					t = parseInt(t, 10); break;
 
-			    // return time
-			    return t;
-		    });
+				default:
+					return;
+			}
+
+			data[key[i]] = t;
+		});
+
+		function sum(a, b) { return a + b; }
+		function seconds(n, i) { return (n || 0) * [86400, 3600, 60][i]; }
+
+		data.percentage = data.remaining / data.maximum * 100;
+		data.bonus      = /Currently in Bonus Period/.test(xhr.responseText);
+		data.bonusIn    = (xhr.responseText.match(/Regular Period Ends in (?:(\d+) days, )?(\d+) hr and (\d+)/) || [])
+		                     .slice(1).map(seconds).reduce(sum, 0);
+		data.regularIn  = (xhr.responseText.match(/Bonus Period Ends in (?:(\d+) days, )?(\d+) hr and (\d+)/) || [])
+		                     .slice(1).map(seconds).reduce(sum, 0);
+		data.resetIn    = xhr.responseText.match(/Allowance Resets in (\d+) days, (\d+) hr and (\d+)/)
+		                     .slice(1).map(seconds).reduce(sum, 0);
 
 
 		// TODO: expand this to have reboot detection later
-		if (typeof data[0] != 'number') {
+		if (typeof data.maximum != 'number') {
 			chrome.browserAction.setTitle({ title: 'HughesNet Allowance Monitor' });
 			chrome.browserAction.setIcon({ path: '../images/error.png' });
 		}
@@ -68,17 +86,17 @@
 		// background
 		ctx.shadowBlur = 2;
 		ctx.shadowColor = 'rgba(0, 0, 0, .6)';
-		ctx.fillStyle = limited ? '#fdd' : '#eee';
+		ctx.fillStyle = data.throttled ? '#fdd' : '#eee';
 		ctx.fillRect(4, 2, 11, 15);
 
 		ctx.shadowBlur = 0;
 
 
 		// usage fill
-		hue = data[2] < v ? (data[2] * (80 / v)) : 80;
+		hue = data.percentage < v ? (data.percentage * (80 / v)) : 80;
 
 		ctx.fillStyle = 'hsl(' + hue + ', 90%, 47%)';
-		ctx.fillRect(4, 17, 11, -((data[2] / 100) * 15));
+		ctx.fillRect(4, 17, 11, -((data.percentage / 100) * 15));
 
 
 		// highlight
@@ -92,20 +110,22 @@
 
 
 		// time bar
-		t = +data[3].split(/:/g).reduce(function(a, b) { return (+a * 60) + (+b); }) / 86400;
+		t = data.resetIn / 86400;
 		ctx.fillStyle = '#aaa';
 		ctx.fillRect(1, 18, 1, -(17 * (1 - t)));
 
 
-		// download zone notification
-		offset = /DT\)$/.test(t = new Date()) ? 4 : 5;
-		if (t.getUTCHours() - offset >= 2 && t.getUTCHours() - offset < 7)
+		// bonus period notification
+		if (data.bonus)
 			ctx.drawImage(dlzone, 9, 9);
 
 		chrome.browserAction.setTitle({
-			title:	(limited ? 'Download speed limited\n' : '') +
-				data[2] + '% (' + data[1] + 'MB/' + data[0] + 'MB)\n' +
-				(data[0] / 2) + 'MB refill in: ' + data[3]
+			title:	(data.throttled ? '!!! Hard-Throttled !!!\n' : '') +
+ 				(data.bonus ?
+				             'Currently in Bonus Period\nRegular Period in: ' +
+				             (new Date(data.regularIn * 1000)).toISOString().substr(11, 8) : 'Bonus Period in: ' + (new Date(data.bonusIn * 1000)).toISOString().substr(11, 8)) + '\n' +
+				data.percentage.toFixed(2) + '% (' + data.remaining + 'MB/' + data.maximum + 'MB)\n' +
+				data.daily + 'MB refill in: ' + (new Date(data.resetIn * 1000)).toISOString().substr(11, 8)
 		});
 
 		chrome.browserAction.setIcon({ imageData: ctx.getImageData(0, 0, 19, 19) });
